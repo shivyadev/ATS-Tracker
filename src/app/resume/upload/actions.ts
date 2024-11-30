ac9805c973ceba366975a5b99f33ba9c29947494
Shivansh <shivanshyadav12@gmail.com>
2024-11-30 22:41:31 +0530

    Added vercel.json

--- src/app/resume/upload/actions.ts
+++ src/app/resume/upload/actions.ts
@@ -8,8 +8,8 @@ import { generateFileName } from "@/lib/crypto";
 const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.NEXT_AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.NEXT_AWS_SECRET_ACCESS_KEY!,
   },
 });
 
